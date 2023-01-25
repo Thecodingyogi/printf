27adf8a986451dac3bf080628df58c46feac3763
Thecodingyogi <cynthiatonui@gmail.com>
2023-01-25 08:58:29 -0800

    corrected

--- _printf.c
+++ _printf.c
@@ -6,37 +6,43 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int count = 0;
-	int num = 0;
+	int i = 0, count = 0, num = 0;
+	int (*f)(va_list);
 	va_list args;
 
 	va_start(args, format);
-	int (*f)(va_list);
 
 	if (format == NULL)
 		return (-1);
 	while (format[i])
 	{
 		if (format[i] != '%')
+		{
 			num = write(1, &format[i], 1);
 			count += num;
 			i++;
 			continue;
+		}
 		if (format[i] == '%')
+		{
 			f = check_specifier(&format[i + 1]);
 			if (f != NULL)
+			{
 				num = f(args);
 				count += num;
 				i = i + 2;
 				continue;
+			}
+			if (format[i + 1] == '\0')
+				break;
 			if (format[i + 1] != '\0')
-				num = write(1, &format[i + 1], 1);
+			{
+				num = write(1, &format[i], 1);
 				count += num;
 				i = i + 2;
 				continue;
-			if (format[i + 1] == '\0')
-				break;
+			}
+		}
 	}
 	return (count);
 }
